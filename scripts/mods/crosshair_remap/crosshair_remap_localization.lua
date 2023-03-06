41bcf862f12d3c19a1d7bea61552342603c08837
deluxghost <deluxghost@users.noreply.github.com>
2023-03-07 02:35:57 +0800

    add zh-cn localization

--- scripts/mods/crosshair_remap/crosshair_remap_localization.lua
+++ scripts/mods/crosshair_remap/crosshair_remap_localization.lua
@@ -33,45 +33,58 @@ end
 local crosshair_name_loc = {
 	assault = {
 		en = "Assault",
+		["zh-cn"] = "突击",
 	},
 	bfg = {
 		en = "BFG",
+		["zh-cn"] = "BFG",
 	},
 	charge_up = {
 		en = "Charge Up",
+		["zh-cn"] = "充能",
 	},
 	charge_up_ads = {
 		en = "Charge Up ADS",
+		["zh-cn"] = "充能机瞄",
 	},
 	cross = {
 		en = "Cross",
+		["zh-cn"] = "十字",
 	},
 	dot = {
 		en = "Dot",
+		["zh-cn"] = "单点",
 	},
 	ironsight = {
 		en = "Ironsight",
+		["zh-cn"] = "机瞄",
 	},
 	projectile_drop = {
 		en = "Projectile Drop",
+		["zh-cn"] = "弹道下坠",
 	},
 	shotgun = {
 		en = "Shotgun",
+		["zh-cn"] = "霰弹",
 	},
 	spray_n_pray = {
 		en = "Spray-n-Pray",
+		["zh-cn"] = "扫射",
 	},
 	none = {
 		en = "None",
+		["zh-cn"] = "无准星",
 	},
 }
 
 local locres = {
 	mod_name = {
 		en = "Crosshair Remap",
+		["zh-cn"] = "准星自定义",
 	},
 	mod_description = {
 		en = "Remap crosshairs.",
+		["zh-cn"] = "重新映射武器准星。",
 	},
 }
 
