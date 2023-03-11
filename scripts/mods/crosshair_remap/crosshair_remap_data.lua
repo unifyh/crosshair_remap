7835fb126fd28399117ac4c06f2c8d4e2327b85e
unifyh <18213435+unifyh@users.noreply.github.com>
2023-03-11 14:18:09 +0800

    Remove outdated code

--- scripts/mods/crosshair_remap/crosshair_remap_data.lua
+++ scripts/mods/crosshair_remap/crosshair_remap_data.lua
@@ -39,25 +39,11 @@ local function make_group(name, default1, default2)
 	}
 end
 
--- local function make_widgets()
--- 	local widgets = {}
--- 	for i, name in ipairs(mod.vanilla_crosshair_names) do
--- 		widgets[i] = {
--- 			setting_id = name .. "_crosshair",
--- 			type = "dropdown",
--- 			default_value = name,
--- 			options = make_options()
--- 		}
--- 	end
--- 	return widgets
--- end
-
 return {
 	name = mod:localize("mod_name"),
 	description = mod:localize("mod_description"),
 	is_togglable = true,
 	options = {
-		-- widgets = make_widgets()
 		widgets = {
 			make_dropdown("none_class", "none"),
 			make_dropdown("melee_class", "dot"),
